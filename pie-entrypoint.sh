7ca30b7063b0aa13a27dda36460470411a5a789f
Stephen J. Butler <stephen.butler@gmail.com>
2016-08-03 22:22:29 -0500

    Better memory limit calculations

--- pie-entrypoint.sh
+++ pie-entrypoint.sh
@@ -9,27 +9,47 @@ if [[ "$1" == "apache2-pie" ]]; then
   # Attempt to set ServerLimit and MaxRequestWorkers based on the amount of
   # memory in the container. This will never use less than 16 servers, and
   # never more than 2000. If no memory limits are specified, then it will
-  # use the lower, conservative defaults.
-  ram_limit=$(</sys/fs/cgroup/memory/memory.limit_in_bytes)
-  swp_limit=$(</sys/fs/cgroup/memory/memory.memsw.limit_in_bytes)
+  # use free space
   exp_memory_size=${PIE_EXP_MEMORY_SIZE:-50}
   res_memory_size=${PIE_RES_MEMORY_SIZE:-50}
 
+  ram_limit=0
+  if [[ -f "/sys/fs/cgroup/memory/memory.limit_in_bytes" ]]; then
+    ram_limit=$(</sys/fs/cgroup/memory/memory.limit_in_bytes)
+    if [[ "$ram_limit" = "9223372036854771712" ]]; then
+      ram_limit=0
+    else
+      ram_limit=$(echo "$ram_limit" | awk '{ print int( $1 / 1048576 ) }')
+    fi
+  fi
+  (( ram_limit <= 0 )) && ram_limit=$(free -m | awk '/^Mem:/ { print $4 }')
+
+  swp_limit=0
+  if [[ -f "/sys/fs/cgroup/memory/memory.memsw.limit_in_bytes" ]]; then
+    swp_limit=$(</sys/fs/cgroup/memory/memory.memsw.limit_in_bytes)
+    if [[ "$swp_limit" = "9223372036854771712" ]]; then
+      swp_limit=0
+    else
+      swp_limit=$(echo "$swp_limit" | awk '{ print int( $1 / 1048576 ) }')
+    fi
+  fi
+  (( swp_limit <= 0 )) && swp_limit=$(free -m | awk '/^Swap:/ { print $4 }')
+
+  tot_limit=$(( ram_limit + swp_limit ))
+
   echoerr "exp_memory_size: $exp_memory_size MB"
   echoerr "res_memory_size: $res_memory_size MB"
-  echoerr "ram_limit: $ram_limit B"
-  echoerr "swp_limit: $swp_limit B"
+  echoerr "ram_limit: $ram_limit MB"
+  echoerr "swp_limit: $swp_limit MB"
+  echoerr "tot_limit: $tot_limit MB"
 
   APACHE_THREADS_PER_CHILD=25
   APACHE_SERVER_LIMIT=16
 
-  if [[ "$ram_limit" != "9223372036854771712" && "$swp_limit" != "9223372036854771712" ]]; then
-    ram_limit=$(echo "$ram_limit" | awk '{ print $1 / 1048576 }')
-    swp_limit=$(echo "$swp_limit" | awk '{ print $1 / 1048576 }')
-
+  if (( tot_limit > 0 )); then
     APACHE_SERVER_LIMIT=$( \
-      echo "$exp_memory_size $res_memory_size $ram_limit $swp_limit" \
-      | awk '{ print int( ($3 + $4 - $2) / $1 ) }' \
+      echo "$exp_memory_size $res_memory_size $tot_limit" \
+      | awk '{ print int( ($3 - $2) / $1 ) }' \
     )
     if (( APACHE_SERVER_LIMIT < 16 )); then
       APACHE_SERVER_LIMIT=16
